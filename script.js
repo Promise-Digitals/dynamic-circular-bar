a4dcccb479e13d1c23bb4cbbd6f17da0df61b4e3
Promise_digitals <promiseedeh025@gmail.com>
2023-11-26 04:34:47 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let progressBar = document.querySelector(".circular-progress");
+let valueContainer = document.querySelector(".value-container");
+let inputValue = document.getElementById("input-value");
+let submitBtn = document.getElementById("submit-btn");
+
+function count(){
+    let progressValue = 0;
+    let counting = inputValue.value;
+    let progressEndValue = +counting;
+    let speed = 60;
+    if (counting <= 0  || counting > 100){
+        progressValue = "";
+    }else{
+        let progress = setInterval(() =>{
+            progressValue++;
+            valueContainer.textContent = `${progressValue}%`;
+            progressBar.style.background = `conic-gradient(#ff004f ${progressValue* 3.6}deg, #555 ${progressValue * 3.6}deg)`;
+            if (progressValue == progressEndValue){
+                clearInterval(progress);
+            }
+        }, speed);
+    }
+}

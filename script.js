b96c2cba5a790d547536803b7893bd9dc176dc13
Promise-Digitals <promiseedeh025@gmail.com>
2024-07-13 22:29:53 +0100

    Updated the design

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ let submitBtn = document.getElementById("submit-btn");
 function count(){
     let progressValue = 0;
     let counting = inputValue.value;
-    let progressEndValue = +counting;
-    let speed = 60;
+    let progressEndValue = +counting; //converting counting string to number...
+    let speed = 100;
     if (counting <= 0  || counting > 100){
         progressValue = "";
     }else{
         let progress = setInterval(() =>{
             progressValue++;
             valueContainer.textContent = `${progressValue}%`;
-            progressBar.style.background = `conic-gradient(#4f76f6 ${progressValue* 3.6}deg, #555 ${progressValue * 3.6}deg)`;
+            progressBar.style.background = `conic-gradient(#6660ff ${progressValue * 3.6}deg, #333 ${progressValue * 3.6}deg)`;
             if (progressValue == progressEndValue){
                 clearInterval(progress);
             }
